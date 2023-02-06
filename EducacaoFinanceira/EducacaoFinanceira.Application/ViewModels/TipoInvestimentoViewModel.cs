7d710a908386fea1067cc1cc417b7e99b2f644cf
xanwerneck <alexandre.andreza@prof.infnet.edu.br>
2023-02-06 11:41:43 -0300

    inclusao de view para lista de investimentos

---
+++ EducacaoFinanceira/EducacaoFinanceira.Application/ViewModels/TipoInvestimentoViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using EducacaoFinanceira.Domain.Entities;
+
+namespace EducacaoFinanceira.Application.ViewModels
+{
+	public class TipoInvestimentoViewModel
+	{
+		public string? Descricao { get; set; }
+		public string? ModeloIrDescricao { get; set; }
+		public TipoInvestimentoViewModel(TipoInvestimento tipoInvestimento)
+		{
+			Descricao = tipoInvestimento.Descricao;
+            ModeloIrDescricao = (tipoInvestimento.ModeloIr != null) ? tipoInvestimento.ModeloIr.Descricao : "Isento";
+		}
+		public static List<TipoInvestimentoViewModel> TiposInvestimento(List<TipoInvestimento> tipoInvestimentos)
+		{
+			var listTiposInvestimento = new List<TipoInvestimentoViewModel>();
+			tipoInvestimentos.ForEach(t => listTiposInvestimento.Add(new TipoInvestimentoViewModel(t)));
+			return listTiposInvestimento;
+		}
+	}
+}
+
---
+++ EducacaoFinanceira/Web/Controllers/TipoInvestimentoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EducacaoFinanceira.Application.Interfaces;
+using EducacaoFinanceira.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Web.Controllers
+{
+    public class TipoInvestimentoController : Controller
+    {
+        private readonly ITipoInvestimentoService _tipoInvestimentoService;
+        public TipoInvestimentoController(ITipoInvestimentoService tipoInvestimentoService)
+        {
+            _tipoInvestimentoService = tipoInvestimentoService;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var tiposInvestimento = _tipoInvestimentoService.GetAll();
+            return View(TipoInvestimentoViewModel.TiposInvestimento(tiposInvestimento.ToList()));
+        }
+    }
+}
+
