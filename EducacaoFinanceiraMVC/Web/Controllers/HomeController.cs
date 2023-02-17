36267810588bd5f504abb4670750d0ec52993029
xanwerneck <alexandre.andreza@prof.infnet.edu.br>
2023-02-17 07:00:21 -0300

    conteudo da aula 6

---
+++ EducacaoFinanceiraMVC/Web/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Domain.Entities;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult FaleConosco()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult FaleConosco(TipoInvestimento tipoInvestimento)
+        {
+            return View(tipoInvestimento);
+        }
+    }
+}
+
