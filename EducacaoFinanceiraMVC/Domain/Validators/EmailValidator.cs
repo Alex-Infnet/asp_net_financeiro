0315a3e231cd2bd90f7f3169898194653f2d5700
xanwerneck <alexandre.andreza@prof.infnet.edu.br>
2023-03-02 19:05:05 -0300

    conteúdo da aula 9

---
+++ EducacaoFinanceiraMVC/Domain/Validators/EmailValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Domain.ViewModels;
+
+namespace Domain.Validators
+{
+    public class EmailValidator : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var FaleConosco = (FaleConoscoViewModel)validationContext.ObjectInstance;
+            if (FaleConosco.Email == null || FaleConosco.Email.Length < 3)
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+            var extensionMail = FaleConosco.Email.Substring(FaleConosco.Email.Length - 3, 3);
+            if (extensionMail != ".br")
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
+
