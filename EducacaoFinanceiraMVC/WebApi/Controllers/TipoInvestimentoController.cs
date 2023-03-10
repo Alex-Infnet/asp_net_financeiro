c25bb77c7c6aa800aaf58c646ac017d9596abfdd
xanwerneck <alexandre.andreza@prof.infnet.edu.br>
2023-03-09 22:08:29 -0300

    conteudo da aula 11

---
+++ EducacaoFinanceiraMVC/WebApi/Controllers/TipoInvestimentoController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Domain.Interfaces;
+using Domain.Entities;
+using Microsoft.AspNetCore.Cors;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.Controllers
+{
+    [DisableCors]
+    [ApiController]
+    public class TipoInvestimentoController : ControllerBase
+    {
+        public ITipoInvestimentoService _service { get; set; }
+        public TipoInvestimentoController(ITipoInvestimentoService service)
+        {
+            _service = service;
+        }
+        [Route("/tipoinvestimento")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok(_service.GetAll());
+        }
+        [Route("/tipoinvestimento/new")]
+        [HttpPost]
+        public IActionResult Create(TipoInvestimento tipoInvestimento)
+        {
+            _service.Create(tipoInvestimento);
+            return NoContent();
+        }
+        [Route("/tipoinvestimento")]
+        [HttpPut]
+        public IActionResult Update(TipoInvestimento tipoInvestimento)
+        {
+            _service.Update(tipoInvestimento);
+            return NoContent();
+        }
+        [Route("/tipoinvestimento/{id}")]
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            _service.Delete(id);
+            return NoContent();
+        }
+    }
+}
+
