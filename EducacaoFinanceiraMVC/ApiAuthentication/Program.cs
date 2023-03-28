a08e72db6eba6b4bad565a10f4570e1357859694
xanwerneck <alexandre.andreza@prof.infnet.edu.br>
2023-03-28 10:56:14 -0300

    conteudo da aula 16

---
+++ EducacaoFinanceiraMVC/ApiAuthentication/Program.cs
@@ -0,0 +1,47 @@
+﻿using ApiAuthentication.Interfaces;
+using ApiAuthentication.Services;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var tokenValidationParams = new TokenValidationParameters()
+{
+    IssuerSigningKey = new SymmetricSecurityKey(TokenService.Key()),
+    ValidateAudience = false,
+    ValidateIssuer = false
+};
+builder.Services.AddAuthentication()
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = tokenValidationParams;
+    });
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IUserService, UserService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
