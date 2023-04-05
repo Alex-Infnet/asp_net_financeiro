a4591f00c0d2e630ba41637d973b66120201387d
xanwerneck <alexandre.andreza@prof.infnet.edu.br>
2023-04-05 06:55:43 -0300

    webidentity settings properties

---
+++ EducacaoFinanceiraMVC/WebIdentity/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Identity;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Service;
+using Domain.Interfaces;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<InvestimentoDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("InvestimentoDb")
+    ));
+
+builder.Services
+    .AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<InvestimentoDbContext>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
+
+app.Run();
+
